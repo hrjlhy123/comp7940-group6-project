6d935d0ee79669b6e0609847849137c708c2c384
HAO Ruojie <hrjlhy@gmail.com>
2022-04-20 16:55:17 +0800

    ultra final version

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+# The code was done by HAO Ruojie (21415315), ZHAO Zimeng (21458839), and Khem LIMBU (21470634).
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 
@@ -6,25 +7,9 @@ import logging
 import redis
 import os
 import re
-# database
+
+# Database
 import mysql.connector
-
-# cnx = mysql.connector.connect(user='doadmin', password='AVNS_IZcLYrdx6q27Ry2',
-#                               host='db-mysql-sgp1-31144-do-user-11210025-0.b.db.ondigitalocean.com',
-#                               port='25060'
-#                               database='defaultdb')
-# cnx.close()
-
-# import configparser
-
-global redis1
-
-#global db_user = os.environ['ACCESS_TOKEN']
-#db_user = 'doadmin'
-#db_psw = 'AVNS_IZcLYrdx6q27Ry2'
-#db_host = 'db-mysql-sgp1-31144-do-user-11210025-0.b.db.ondigitalocean.com'
-#db_port = '25060'
-#db_db = 'defaultdb'
 
 db_user = os.environ['db_user']
 db_psw = os.environ['db_psw']
@@ -33,33 +18,25 @@ db_port = os.environ['db_port']
 db_db = os.environ['db_db']
 
 def main():
-    # Load your token and create an Updater for your Bot
-    
-    # config = configparser.ConfigParser()
-    # config.read('config.ini')
+    # Load token and create an Updater
     updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-
-    global redis1
-    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
-
-    # You can set this logging module, so you will know when and why things do not work as expected
+    
+    # Set logging module to know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
     
-    # register a dispatcher to handle message: here we register an echo dispatcher
+    # Register a dispatcher to handle message: here we register an echo dispatcher
     echo_handler = MessageHandler(Filters.text & (~Filters.command) & (Filters.regex(re.compile(r'hi', re.IGNORECASE)) | Filters.regex(re.compile(r'hello', re.IGNORECASE))), echo)
     dispatcher.add_handler(echo_handler)
 
-    # register a dispatcher to handle message: here we register an echo dispatcher
+    # Register a dispatcher to handle message: here we register an echo dispatcher
     movie_handler = MessageHandler(Filters.text & (~Filters.command) & (Filters.regex(re.compile(r'movie', re.IGNORECASE)) | Filters.regex(re.compile(r'film', re.IGNORECASE)) | Filters.regex(re.compile(r'cinematic', re.IGNORECASE)) | Filters.regex(re.compile(r'program', re.IGNORECASE)) | Filters.regex(re.compile(r'show', re.IGNORECASE)) | Filters.regex(re.compile(r'episode', re.IGNORECASE)) | Filters.regex(re.compile(r'segment', re.IGNORECASE))), movie_command)
     dispatcher.add_handler(movie_handler)
 
-    # on different commands - answer in Telegram
-    # dispatcher.add_handler(CommandHandler("add", add))
+    # On different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("hello", hello_command))
-    # /movie Horror -> Scary Movie
     dispatcher.add_handler(CommandHandler("movie", movie_command))
     dispatcher.add_handler(CallbackQueryHandler(movie_reply_command))
     dispatcher.add_handler(CommandHandler("movie_search", movie_search_command))
@@ -75,20 +52,6 @@ def echo(update, context):
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-# def add(update: Update, context: CallbackContext) -> None:
-#     """Send a message when the command /add is issued."""
-#     try: 
-#         global redis1
-#         logging.info(context.args[0])
-#         msg = context.args[0]   # /add keyword <-- this should store the keyword
-#         redis1.incr(msg)
-#         update.message.reply_text('You have said ' + msg +  ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
-#     except (IndexError, ValueError):
-#         update.message.reply_text('Usage: /add <keyword>')
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
@@ -142,7 +105,6 @@ def movie_reply_command(update, context):
                         port=db_port,
                         database=db_db)
     cursor = cnx.cursor()
-    # print("update: " + update)
     print("context.callback_query.data: " + update.callback_query.data)
     query = ("SELECT Title, Content FROM MOVIES WHERE Content ='" + update.callback_query.data +"'")
     cursor.execute(query)
@@ -164,21 +126,16 @@ def movie_search_command(update: Update, context: CallbackContext) -> None:
                             port=db_port,
                             database=db_db)
         cursor = cnx.cursor()
-        # print("Works well!1")
         query = ("SELECT Title, Content FROM MOVIES " 
         "WHERE Content ='" + context.args[0]+ "'")
-        # print("Works well!2")
         cursor.execute(query)
-        # print("Works well!3")
         for (i) in cursor:
             update.message.reply_text("Movie: {}".format(i[0]))
-            # print("Works well!for")
         cursor.close()
         cnx.close()
-        # print("Works well!end")
     except (IndexError, ValueError):
-        # User: Give me horor movie/ Shows me horor movie. User: /movie horor
-        update.message.reply_text('Usage: /movie <keyword>')
+        # User: Give me horor movie/ Shows me horor movie. User: /movie_search horor
+        update.message.reply_text('Usage: /movie_search <keyword>')
 
 def movie_add_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /movie_add is issued."""
@@ -191,7 +148,6 @@ def movie_add_command(update: Update, context: CallbackContext) -> None:
                             host=db_host,
                             port=db_port,
                             database=db_db)
-        #cnx.enter_transaction_management()
         cursor = cnx.cursor()
         print("cursor done")
         insertSql = ("INSERT INTO MOVIES (Title, Content) VALUES ('" + title + "', '" + content + "')")
@@ -206,22 +162,14 @@ def movie_add_command(update: Update, context: CallbackContext) -> None:
                 print("Title: {}, Content: {}".format(i[0], i[1]))
         except:
             cnx.rollback()
-        
-        #cursor.close()
-        #cnx.close()
         print("Works well!end")
     except (IndexError, ValueError, DatabaseError):
-        # User: Give me horor movie/ Shows me horor movie. User: /movie horor
-        update.message.reply_text('Usage: /movie <keyword>')
+        # User: Add a movie. User: /movie_add movieTitle_moviecontent
+        update.message.reply_text('Usage: /movie_add <keyword>')
     finally:
         if cursor:
             cursor.close()
         cnx.close()
-        #cnx.leave_transaction_management()
-
-# The code was done by Ruojie Hao (Student ID: 21415315).
 
 if __name__ == '__main__':
     main()
-
-
